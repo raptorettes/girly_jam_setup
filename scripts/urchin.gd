b7405bd0f2112fa15dd7c6c3fd097ef37b73e069
Rapt0rettes <rene@raptor.ink>
2026-05-23 16:39:13 +0200

    urchin, lettuce and imports

---
+++ scripts/urchin.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var urchin_sprites: Array[Texture2D] 
+
+var growth_stage := 0
+
+@onready var sprite := $UrchinSprite
+@onready var growth_timer := $GrowthTimer
+
+
+func _ready():
+	sprite.texture = urchin_sprites[0]
+	growth_timer.wait_time = 5.0
+	growth_timer.start()
+
+
+func _on_growth_timer_timeout():
+	if growth_stage < 2:
+		growth_stage += 1
+		sprite.texture = urchin_sprites[growth_stage]
+	if growth_stage == 2:
+		growth_timer.stop()  # fully grown, stop timer
+
+
+#
+#func _on_urchin_spawn_timer_timeout() -> void:
+	#GameManager.spawn_urchin(global_position)
