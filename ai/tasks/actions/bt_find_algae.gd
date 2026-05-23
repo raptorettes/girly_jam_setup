1fd89eb94ac34f28971e8e9dffc2621d3dd03a7b
Matthew McLeod <m@qwrk.dev>
2026-05-23 23:03:29 +0200

    fix: creb hungers

--- ai/tasks/actions/bt_find_algae.gd
+++ ai/tasks/actions/bt_find_algae.gd
@@ -2,13 +2,13 @@ extends BTAction
 
 func _tick(delta: float) -> Status:
 	var algae_list = agent.get_tree().get_nodes_in_group("algae")
-	print("algae found: ", algae_list.size())
 	if algae_list.is_empty():
 		return FAILURE
-	var crab = agent as CharacterBody2D
-	var nearest = algae_list.reduce(func(a, b): 
+	var crab = agent as Crabby
+	var urchind = algae_list.filter(func(a: Algae): return a.urchin_popuplation > 0)
+	urchind.sort_custom(func(a, b): 
 		return a if crab.global_position.distance_to(a.global_position) < crab.global_position.distance_to(b.global_position) else b
 	)
-	blackboard.set_var("target_algae", nearest)
-	print("target set: ", nearest)
+	var nearest : Algae = urchind.front()
+	crab.set_target(nearest)
 	return SUCCESS
--- scripts/crab.gd
+++ scripts/crab.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
+class_name Crabby
 
 @onready var nav_agent := $NavigationAgent2D
 
 const SPEED = 50.0
+var age: int = 0
+var target: Algae 
 
 func move_to(target_pos: Vector2):
 	nav_agent.target_position = target_pos
 
+func set_target(t: Algae) -> void:
+	target = t
+	
+func eat_algae() -> void:
+	target.kill_urchin()
+	
 func _physics_process(delta):
 	if nav_agent.is_navigation_finished():
 		return
